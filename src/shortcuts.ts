615c2ced36ea70c6420260260bb161cd1927e8ee
chris-zhu <1633711653@qq.com>
2022-06-08 18:04:32 +0800

    feat: add shortcuts

--- src/shortcuts.ts
+++ src/shortcuts.ts
@@ -4,6 +4,7 @@ const shortcuts: UserShortcuts = [
   ['pr', 'relative'],
   ['pa', 'absolute'],
   ['pf', 'fixed'],
+  ['p-c', 'pa top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'],
   ['f-c', 'flex justify-center items-center'],
   ['f-c-c', 'f-c flex-col'],
 
@@ -23,7 +24,14 @@ const shortcuts: UserShortcuts = [
   ['fic', 'flex items-center'],
   ['fccc', 'flex justify-center items-center flex-col'],
 
-  ['p-c', 'pa top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'],
+  ['trans', 'transition-all-350 ease-linear'],
+  ['text', 'text-text-default dark:text-text-dark'],
+  ['bg', 'trans bg-bg-default dark:bg-bg-dark'],
+  ['base', 'trans text'],
+
+  ['icon', 'w-5.5 h-5.5 cursor-pointer select-none transition-opacity-300 ease-in-out text'],
+  ['icon-btn', 'icon color-inherit op64 hover-op100 hover-color-teal-500 dark-hover-color-inherit'],
+  ['icon-link', 'icon color-inherit op64 hover:op100 hover-text-red-300 dark-hover-color-inherit'],
 ]
 
 export const generateShortcuts = (ruleMeta: RuleMeta) => {
