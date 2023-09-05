8cda39fe72241a7d29a333058cecfc27303a94e7
Chris <hizyyv@gmail.com>
2023-09-06 00:02:56 +0800

    chore: update

--- uno.config.ts
+++ uno.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig, presetUno } from 'unocss'
+import { defineConfig } from 'unocss'
 import { presetUseful } from './dist'
 
 // Just for Vscode Extension
 
 export default defineConfig({
   presets: [
-    presetUno(),
     presetUseful(),
   ],
 })
