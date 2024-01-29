edd8668d5a229510ff04406c54d4ae2ab755dd8c
Chris <hizyyv@gmail.com>
2024-01-29 17:12:20 +0800

    chore: update

--- packages/core/build.config.ts
+++ packages/core/build.config.ts
@@ -12,34 +12,5 @@ export default defineBuildConfig({
   externals: [
     'unocss',
   ],
-  // TOOD: import css as string
-  // hooks: {
-  //   'rollup:options': (_, options) => {
-  //     options.plugins = toArray(options.plugins ?? [])
-  //     options.plugins!.push(
-  //       string({
-  //         include: ['**/*.css'],
-  //       }),
-  //     )
-  //   },
-  // },
 })
 
-// function string(opts: any): InputPluginOption {
-//   if (!opts.include)
-//     throw new Error('include option should be specified')
-
-//   const filter = createFilter(opts.include, opts.exclude)
-
-//   return {
-//     name: 'string',
-//     transform(code, id) {
-//       if (filter(id)) {
-//         return {
-//           code: `export default ${JSON.stringify(code)};`,
-//           map: { mappings: '' },
-//         }
-//       }
-//     },
-//   }
-// }
