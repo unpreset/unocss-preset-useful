ab50a88c1b296b5ac84223933d8e425be7e40104
Chris <hizyyv@gmail.com>
2024-03-12 14:26:20 +0800

    fix(playground): enable magic animates

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -4,5 +4,8 @@ import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), UnoCSS()],
+  plugins: [
+    vue(),
+    UnoCSS('../uno.config.ts'),
+  ],
 })
--- uno.config.ts
+++ uno.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   presets: [
     presetUseful({
       typography: true,
+      enableMagicAnimations: true,
     }),
   ],
 })
