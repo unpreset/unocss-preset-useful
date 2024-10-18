9c2692050a1d3d413dd6d12f9a0f7d83f73c65c2
Chris <hizyyv@gmail.com>
2024-10-18 18:19:49 +0800

    chore: playground apply

--- playground/uno.config.ts
+++ playground/uno.config.ts
@@ -1,18 +1,13 @@
-import { defineConfig } from 'unocss'
-import { presetUseful } from 'unocss-preset-useful'
+import { defineUsefulConfig } from 'unocss-preset-useful'
 
-export default defineConfig({
-  presets: [
-    presetUseful({
-      typography: true,
-      enableMagicAnimations: true,
-      webFonts: {
-        fonts: {
-          dm: 'DM Sans',
-          mono: 'Fira Code',
-          roboto: 'Roboto Condensed',
-        },
-      },
-    }),
-  ],
+export default defineUsefulConfig({
+  typography: true,
+  enableMagicAnimations: true,
+  webFonts: {
+    fonts: {
+      dm: 'DM Sans',
+      mono: 'Fira Code',
+      roboto: 'Roboto Condensed',
+    },
+  },
 })
